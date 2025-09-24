c8c1bd7fd77ca6c928adbe057a407fd87571d4c7
LoongPig <DragonPigLZY@163.com>
2025-09-24 19:55:11 +0800

    Update linkData.mjs

--- .vitepress/theme/assets/linkData.mjs
+++ .vitepress/theme/assets/linkData.mjs
@@ -7,40 +7,10 @@ const linkData = [
     typeDesc: "都是大佬，推荐关注",
     typeList: [
       {
-        name: "WL",
-        avatar: "https://cdn.luogu.com.cn/upload/usericon/851495.png",
-        desc: "《南谷的往事与未来》作者",
-        url: "https://www.luogu.com.cn/user/851495",
-      },
-      {
-        name: "一只小菜包",
-        avatar: "https://cdn.luogu.com.cn/upload/usericon/907233.png",
-        desc: "少年陈哲原作者",
-        url: "https://www.luogu.com.cn/user/907233",
-      },
-      {
         name: "CodingOIer",
         avatar: "https://cdn.luogu.com.cn/upload/usericon/754324.png",
         desc: "OI 大佬，洛谷工具站建立者",
         url: "https://www.luogu.com.cn/user/754324",
-      },
-      {
-        name: "Ender_pearl2333",
-        avatar: "https://cdn.luogu.com.cn/upload/usericon/1167457.png",
-        desc: "tsh朋友，音游大佬",
-        url: "https://www.luogu.com.cn/user/1167457",
-      },
-      {
-        name: "lfxxx",
-        avatar: "https://cdn.luogu.com.cn/upload/usericon/795344.png",
-        desc: "OI&zorr 大佬",
-        url: "https://www.luogu.com.cn/user/795344",
-      },
-      {
-        name: "Vocaloid世末歌者",
-        avatar: "https://cdn.luogu.com.cn/upload/usericon/678881.png",
-        desc: "石门少科院成员，OI 巨佬",
-        url: "https://www.luogu.com.cn/user/678881",
       },
     ],
   },
@@ -54,12 +24,6 @@ const linkData = [
         avatar: "https://lijingshu2014.github.io/",
         desc: "lijingshu 的个人博客",
         url: "https://lijingshu2014.github.io/",
-      },
-      {
-        name: "ChasonWang's Blog",
-        avatar: "https://cdn.luogu.com.cn/upload/image_hosting/rl6d1w19.png",
-        desc: "一个热爱摸鱼的 OIer 的博客",
-        url: "https://chasonwang2012.github.io",
       },
     ],
   },
